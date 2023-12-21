3d1a29628acf8285e993f57a2f67c7411596a263
ColettesCorvette <th.fchs@gmail.com>
2023-12-21 11:17:07 +0100

    gg

---
+++ thomas/testEtudiant/PaquetCartes.java
@@ -0,0 +1,147 @@
+import java.util.Random;
+
+public class PaquetCartes {
+    private Carte[] cartes;
+
+    public PaquetCartes() {
+        this.cartes = new Carte[0];
+    }
+
+    public PaquetCartes(Carte[] paquet){
+        this.cartes = paquet;
+    }
+
+    public PaquetCartes(int[] paquet) {
+
+        this.cartes = new Carte[paquet.length];
+        for(int i = 0; i < paquet.length; i++){
+            this.cartes[i] = new Carte(paquet[i]);
+        }
+    }
+
+    
+
+    public int getNbCartes() {
+        return this.cartes.length;
+    }
+
+    public Carte getCarte(int place) {
+        if (place < 0 || place > this.getNbCartes() - 1) {
+            return null;
+        } else {
+            return this.cartes[place];
+        }
+    }
+
+    public Carte getDerniereCarte() {
+        if (this.etreVide()) {
+            return null;
+        } else {
+            return this.cartes[this.getNbCartes() - 1];
+        }
+    }
+
+    public boolean etreVide() {
+        return (getNbCartes() == 0);
+    }
+
+    public void ajouterCarteFin(Carte nvlCarte) {
+        int n = this.getNbCartes();
+        Carte[] tempCartes = new Carte[n+1];
+        for (int i = 0; i < n; i++) {
+            tempCartes[i] = this.cartes[i];
+        }
+        tempCartes[n] = nvlCarte;
+        this.cartes = tempCartes;
+    }
+
+    public Carte retirerCarte(int place) {
+        if (place < 0 || place > this.getNbCartes() - 1) {
+            return null;
+        } else {
+            int n = this.getNbCartes();
+            Carte carteSup = this.cartes[place];
+            Carte[] tempCartes = new Carte[n - 1];
+
+            for (int i = 0; i < place; i++) {
+                tempCartes[i] = this.cartes[i];
+            }
+
+            for (int i = place + 1; i < n; i++) {
+                tempCartes[i - 1] = this.cartes[i];
+            }
+
+            this.cartes = tempCartes;
+            return carteSup;
+        }
+    }
+
+    public String toString() {
+        String res = "";
+        for (int i = 0; i < this.getNbCartes()-1; i++) {
+            res += i + "-" + this.cartes[i] + " ";
+        }
+        res+=this.getNbCartes()-1+"-"+this.getDerniereCarte();
+        return res;
+    }
+
+    public void remplir(int max) {
+        Carte[] tab = new Carte[max - 2];
+        for (int i = 0; i < tab.length; i++) {
+            tab[i] = new Carte(i + 2);
+        }
+        this.cartes = tab;
+    }
+
+    private Carte piocherHasard() {
+        Random r = new Random();
+        int i = r.nextInt(getNbCartes());
+        return this.retirerCarte(i);
+    }
+
+    public void melangerPaquet() {
+        Carte[] tab = new Carte[this.getNbCartes()];
+        for (int i = 0; i < this.getNbCartes(); i++) {
+            tab[i] = piocherHasard();
+        }
+        this.cartes = tab;
+    }
+
+    public Carte prendreCarteDessus() {
+        if (etreVide()) {
+            return null;
+        } else {
+            return this.retirerCarte(0);
+        }
+    }
+
+    public void insererTri(Carte c) {
+        Carte[] tab = new Carte[this.getNbCartes() + 1];
+        if (this.getNbCartes() != 0) {
+            boolean insere = false;
+            int i = 0;
+            while (! insere && i < this.getNbCartes()-1) {
+                if (c.etrePlusGrand(this.cartes[i])) {
+                    tab[i] = this.cartes[i];
+                } else {
+                    tab[i] = c;
+                    insere = true;
+                }
+                i++;
+            }
+            for (int j = i; j < tab.length; j++) {
+                tab[j] = this.cartes[j - 1];
+            }
+            if(!insere){
+                tab[tab.length-1]=c;
+            }
+        } else {
+            tab[0] = c;
+        }
+        
+        this.cartes = tab;
+
+
+    
+    }
+}
--- thomas/testEtudiant/TestPaquetCartes.java
+++ thomas/testEtudiant/TestPaquetCartes.java
@@ -1,4 +1,5 @@
 import static libtest.Lanceur.lancer;
+import static libtest.OutilTest.*;
 
 public class TestPaquetCartes {
 
@@ -38,6 +39,224 @@ public class TestPaquetCartes {
         Carte c3 = paq.prendreCarteDessus();
 
     }
+
+    public void test_1_verifieConstructeurVide(){
+        
+        PaquetCartes paq1 = new PaquetCartes();
+
+        int l = paq1.getNbCartes();
+
+        //On teste si le constructeur instancie bien un tableau vide
+        assertEquals("Le paquet de carte doit être vide par défaut",0,l);
+
+    }
+
+    public void test_2_verifieConstructeurParam(){
+        Carte[] tabC = new Carte[2];
+        tabC[0]= new Carte(1);
+        tabC[1]= new Carte(2);
+
+        PaquetCartes paq1 = new PaquetCartes(tabC);
+
+        int l = paq1.getNbCartes();
+
+        //On verifie le constructeur avec parametre
+        assertEquals("Le paquet devrait avoir 2 cartes",2,l);
+    }
+
+    
+    public void test_3_ajouterCarteFin(){
+
+        Carte[] tabC = new Carte[2];
+        tabC[0]= new Carte(2);
+        tabC[1]= new Carte(4);
+        PaquetCartes paq1 = new PaquetCartes(tabC);
+
+        Carte c = new Carte(6);
+
+        paq1.ajouterCarteFin(c);
+
+        // On verifie que le paquet a ete agrandi et que la carte a bien ete ajoutee a la fin
+        assertEquals("La carte doit être à la fin",c,paq1.getDerniereCarte());
+        assertEquals("le paquet devrait etre plus grand de 1",3, paq1.getNbCartes());
+
+
+    }
+
+    public void test_4_ajouterCarteFin_vide(){
+
+        Carte[] tabC = new Carte[0];
+
+        PaquetCartes paq1 = new PaquetCartes(tabC);
+
+        Carte c = new Carte(1);
+
+        paq1.ajouterCarteFin(c);
+
+        //On verifie si la taille du paquet augmente et que la carte ajoutee est la bonne
+        assertEquals("La taille devrait etre de 1",1, paq1.getNbCartes());
+        assertEquals("La carte inseree devrait la premier",c, paq1.getDerniereCarte());
+
+
+
+    }
+
+    public void test_5_retirerCarte(){
+
+        Carte[] tabC = new Carte[2];
+        tabC[0]=new Carte(1);
+        tabC[1]= new Carte(2);
+        PaquetCartes pq1 = new PaquetCartes(tabC);
+
+        pq1.retirerCarte(1);
+
+        //On teste si la taille du paquet est reduite de 1 et que la carte retiree est la bonne
+        assertEquals("La doit du paquet devrait être 1",1,pq1.getNbCartes());
+        assertEquals("La carte retiree n'est pas la bonne",tabC[0],pq1.getDerniereCarte() );
+
+    }
+
+    public void test_6_toString(){
+
+        Carte[] tabC = {new Carte(1),new Carte(2),new Carte(3),new Carte(4)};
+
+        PaquetCartes paq1 = new PaquetCartes(tabC);
+
+        //On verifie que la methode toString renvoie le format attendu
+        assertEquals("la methode n'affiche le bon resultat","0-c1 1-c2 2-c3 3-c4",paq1.toString());
+    
+
+    }
+
+    public void test_7_melangerPaquet(){
+        
+        Carte[] tabC = new Carte[5];
+        tabC[0]= new Carte(1);
+        tabC[1]= new Carte(2);
+        tabC[2]= new Carte(3);
+        tabC[3]= new Carte(4);
+        tabC[4]= new Carte(5);
+
+        PaquetCartes paq1 = new PaquetCartes(tabC);
+
+        paq1.melangerPaquet();
+
+        //On a teste si la taille du paquet demeurre le meme apres avoir melange le paquet de cartes
+        //A noter que si l'assertEquals (pas fourni ici) qui verifie l'egalite de l'indice avec la valeur changee echoue, 
+        //c'est le signe que la methode fontion
+        assertEquals("la taille n'est pas la meme",5, paq1.getNbCartes());
+
+
+
+    }
+
+    public void test_8_insererTri(){
+
+        Carte[] tabC = new Carte[3];
+        tabC[0]= new Carte(12);
+        tabC[1]= new Carte(32);
+        tabC[2]= new Carte(45);
+
+    
+        PaquetCartes paq1 = new PaquetCartes(tabC);
+        Carte c = new Carte(23);
+
+        paq1.insererTri(c);
+
+        //On teste si la methode de tri a l'insertion fonctionne, en verifiant si la taille augmente et la valeur
+        assertEquals("Le paquet devrait etre plus grand 1",4,paq1.getNbCartes());
+        assertEquals("La carte n'est pas bien placee",c,paq1.getCarte(1));
+
+
+    }
+
+    public void test_9_prendreCarteDessus(){
+
+        Carte[] tabC = new Carte[3];
+        tabC[0]= new Carte(1);
+        tabC[1]= new Carte(2);
+        tabC[2]= new Carte(3);
+
+        PaquetCartes paq1 = new PaquetCartes(tabC);
+
+        paq1.prendreCarteDessus();
+
+        //On verifie que la carte a l'indice 0 est retiree du paquet et que sa taille diminue 
+        assertEquals("La carte n'a pas ete retiree",2,paq1.getNbCartes());
+        assertEquals("La premiere carte n'a pas ete retiree",tabC[1], paq1.getCarte(0));
+
+
+    }
+
+    public void test_10_prendreCarteDessus_vide(){
+
+        Carte[] tabC = new Carte[0];
+
+        PaquetCartes paq1 = new PaquetCartes(tabC);
+
+        paq1.prendreCarteDessus();
+
+        //On verifie que rien ne se passe lorsque le paquet est deja vide
+        assertEquals("Il ne devrait pas y avoir de carte a prendre",null, paq1.getCarte(0));
+        assertEquals("La taille devrait etre de 0",0, paq1.getNbCartes());
+
+    }
+
+    public void test_11_insererTri_Debut(){
+        Carte[] tabC = new Carte[3];
+        tabC[0]= new Carte(12);
+        tabC[1]= new Carte(32);
+        tabC[2]= new Carte(45);
+
+    
+        PaquetCartes paq1 = new PaquetCartes(tabC);
+        Carte c = new Carte(5);
+
+        paq1.insererTri(c);
+
+        //On verifie l'insertion d'une carte au debut
+        assertEquals("Le paquet devrait etre plus grand 1",4,paq1.getNbCartes());
+        assertEquals("La carte n'est pas bien placee",c,paq1.getCarte(0));
+
+
+    }
+
+    public void test_12_insererTri_Fin(){
+        Carte[] tabC = new Carte[3];
+        tabC[0]= new Carte(12);
+        tabC[1]= new Carte(32);
+        tabC[2]= new Carte(45);
+
+    
+        PaquetCartes paq1 = new PaquetCartes(tabC);
+        Carte c = new Carte(56);
+
+        paq1.insererTri(c);
+
+        //On verifie l'insertion d'une carte a la fin
+        assertEquals("Le paquet devrait etre plus grand 1",4,paq1.getNbCartes());
+        assertEquals("La carte n'est pas bien placee",c,paq1.getCarte(3));
+
+
+    }
+
+    public void test_13_insererTri_Vide(){
+
+        Carte[] tabC = new Carte[0];
+        
+    
+        PaquetCartes paq1 = new PaquetCartes(tabC);
+        Carte c = new Carte(4);
+
+        paq1.insererTri(c);
+
+        //On verifie l'insertion d'une carte lorsque le paquet est vide
+        assertEquals("Le paquet devrait etre plus grand 1",1,paq1.getNbCartes());
+        assertEquals("La carte n'est pas bien placee",c,paq1.getCarte(0));
+
+
+    }
+    
 
     /**
      * lancement des tests
