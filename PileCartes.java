ac98bf37bd47d5003c05d7121d0897bec5b04910
Packingdustry <alois.mc@hotmail.com>
2023-12-21 10:26:06 +0100

    méthode etrePosable()

--- PileCartes.java
+++ PileCartes.java
@@ -7,9 +7,14 @@ public class PileCartes {
     private boolean croissant;
     private PaquetCartes paquet;
 
+    /**
+     * Constructeur de PileCartes
+     * @param pCroissant boolean, vrai si la pile est croissante
+     * @param max int, valeur de la plus grande carte et taille max de la pile
+     */
     public PileCartes(boolean pCroissant, int max) {
         this.croissant = pCroissant;
-        Carte[] pCartes = new Carte[max - 2];
+        Carte[] pCartes = new Carte[1];
         if (pCroissant) {
             pCartes[0] = new Carte(1);
         } else {
@@ -17,4 +22,20 @@ public class PileCartes {
         }
         this.paquet = new PaquetCartes(pCartes);
     }
+
+    /**
+     * vérifie si la carte passée en paramètre est posable en haut de la pile
+     * @param c Carte à ajouter
+     * @return boolean, vrai si il est possible d'ajouter la carte
+     */
+    public boolean etrePosasble(Carte c) {
+        boolean posable = false;
+        if ((this.croissant && c.etrePlusGrand(paquet.getDerniereCarte())) || 
+            (! this.croissant && ! c.etrePlusGrand(paquet.getDerniereCarte()))) {
+                posable = true;
+        }
+        return posable;
+    }
+
+    
 }
